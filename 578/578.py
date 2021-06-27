5bcc2ab31285d801f14a394ff71efcae724831fb
ichbindev <cmlinac@gmail.com>
2021-06-27 15:24:33 -0500

    python implementation of 578: array contains duplicates

---
+++ 578/578.py
@@ -0,0 +1,8 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+        for i in range(len(nums)):
+            if nums[i] in seen:
+                return True
+            seen.add(nums[i])
+        return False
