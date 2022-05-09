1184e1ad55f32cd3e5c4f3f8a45ca3a7f0bb8f8b
ichbindev <cmlinac@gmail.com>
2022-05-09 17:13:48 -0500

    add max subarray description

--- grind75/maxSubarraySum/maxSubarraySum.ts
+++ grind75/maxSubarraySum/maxSubarraySum.ts
@@ -1,3 +1,8 @@
+/*
+Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+A subarray is a contiguous part of an array.
+*/
 function maxSubArray(nums: number[]): number {
   let maxSubarraySum = -Infinity,
       currentSum = 0;
