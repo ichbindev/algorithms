c97b14d6f5ccaebc844f4ef771c4ad21f7446590
ichbindev <cmlinac@gmail.com>
2022-05-15 13:08:07 -0500

    clean up mergeTwoLists

--- grind75/mergeSortedLists/mergeSortedLists.ts
+++ grind75/mergeSortedLists/mergeSortedLists.ts
@@ -20,24 +20,19 @@ Return the head of the merged linked list.
  function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
   let head: ListNode | null = new ListNode(),
       current = head;
+
   while (list1 || list2) {
-      if (!list1) {
-          current.next = list2;
-          list2 = list2.next;
-      } else if (!list2) {
+      if (!list2 || list1 && list1.val < list2.val) {
           current.next = list1;
           list1 = list1.next;
-      } else {
-          if (list1.val < list2.val) {
-              current.next = list1;
-              list1 = list1.next;
-          } else {
-              current.next = list2;
-              list2 = list2.next;
-          }
       }
+      else {
+          current.next = list2;
+          list2 = list2.next;
+      }  
       current = current.next;
   }
+  
   return head.next;
 };
   
