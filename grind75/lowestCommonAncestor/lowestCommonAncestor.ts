7f9d97dde17dfe0d8ca5805509f9cd47f5508eda
ichbindev <cmlinac@gmail.com>
2022-05-17 17:04:45 -0500

    grind75 lowest common ancestor

---
+++ grind75/lowestCommonAncestor/lowestCommonAncestor.ts
@@ -0,0 +1,30 @@
+/*
+Given a binary search tree (BST), find the lowest common ancestor (LCA) of two given nodes in the BST.
+
+According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+*/
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+ function lowestCommonAncestor(root: TreeNode | null, p: TreeNode | null, q: TreeNode | null): TreeNode | null {
+  // if both are left or both are right of root, visit it and try again
+  if (root.val > p.val && root.val > q.val) {
+      return lowestCommonAncestor(root.left, p, q);
+  }
+  if (root.val < p.val && root.val < q.val) {
+      return lowestCommonAncestor(root.right, p, q);
+  }
+  // otherwise the current node is the LCA
+return root;
+};
