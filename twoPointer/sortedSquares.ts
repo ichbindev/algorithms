46072384b0f5579990929666549f2bc134e13ca6
ichbindev <cmlinac@gmail.com>
2022-05-25 17:43:41 -0500

    added alternate solution for sorted squares that won't require an array.reverse or a bunch of array.unshift

--- twoPointer/sortedSquares.ts
+++ twoPointer/sortedSquares.ts
@@ -18,3 +18,21 @@ function sortedSquares(nums: number[]): number[] {
 
   return result.reverse();
 };
+
+function sortedSquaresNoReverse(nums: number[]): number[] {
+  let left = 0,
+      right = nums.length - 1,
+      result = Array(nums.length).fill(0);
+
+  for (let i = nums.length - 1; i >= 0; i--) {
+    if (Math.abs(nums[left]) >= Math.abs(nums[right])) {
+      result[i] = nums[left] ** 2;
+      ++left;
+    } else {
+      result[i] = nums[right] ** 2;
+      --right;
+    }
+  }
+
+  return result;
+}
