9b3edf72f27a4ef012149048db62d1028b7eea97
ichbindev <cmlinac@gmail.com>
2022-05-25 17:36:08 -0500

    sorted squares, way cleaner

--- twoPointer/sortedSquares.ts
+++ twoPointer/sortedSquares.ts
@@ -2,32 +2,19 @@
 Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
 */
 function sortedSquares(nums: number[]): number[] {
-  let firstNonNegative = nums.findIndex(x => x >= 0),
-        right = firstNonNegative >= 0 ? firstNonNegative : nums.length,
-        left = right - 1,
-        result = [];
-  
-  while (left >= 0 || right < nums.length) {
-      if (left >= 0 && right < nums.length) {
-          if (Math.abs(nums[left]) <= nums[right]) {
-              const squared = nums[left] * nums[left];
-              result.push(squared);
-              --left;
-          } else {
-              const squared = nums[right] * nums[right];
-              result.push(squared);
-              ++right;
-          }
-      } else if (left >= 0) {
-          const squared = nums[left] * nums[left];
-              result.push(squared);
-              --left;
-      } else {
-          const squared = nums[right] * nums[right];
-              result.push(squared);
-              ++right;
-      }
+  let left = 0,
+      right = nums.length - 1,
+      result = [];
+
+  while (left <= right) {
+    if (Math.abs(nums[left]) >= Math.abs(nums[right])) {
+      result.push(nums[left] ** 2);
+      ++left;
+    } else {
+      result.push(nums[right] ** 2);
+      --right;
+    }
   }
-  
-  return result;
+
+  return result.reverse();
 };
