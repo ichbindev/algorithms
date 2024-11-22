92bc21235f1da96998ca9bcf2cfc316b2cbdc043
ichbindev <cmlinac@gmail.com>
2024-11-22 16:46:21 -0600

    add prompt and note to optimal freelancing

--- algoexpert/optimalFreelancing/optimalFreelancingSuboptimal.js
+++ algoexpert/optimalFreelancing/optimalFreelancingSuboptimal.js
@@ -1,8 +1,32 @@
+/*
+  You recently started freelance software development and have been offered
+  a variety of job opportunities. Each job has a deadline, meaning there is no
+  value in completing the work after the deadline. Additionally, each job
+  has an associated payment representing the profit for completing that job.
+  Given this information, write a function that returns the maximum profit that
+  can be obtained in a 7-day period.
+
+  Each job will take 1 full day to complete, and the deadline will be given
+  as the number of days left to complete the job. For example, if a job has a
+  deadline of 1, then it can only be completed if it is the first job worked
+  on. If a job has a deadline of 2, then it could be started on the first or
+  second day.
+  
+  Note: There is no requirement to complete all of the jobs. Only one job can
+  be worked on at a time, meaning that in some scenarios it will be impossible
+  to complete them all.
+*/
+
 function optimalFreelancing(jobs) {
+  // worth noting that this solution assumes you will work day 1 - 7
+  // so it's finding optimal solutions in that range and NOT the best 
+  // 7 day window of given jobs (prompt didn't specify, sliding window 
+  // solution does not pass tests)
+  const NUMBER_OF_DAYS = 7;
   let pay = 0;
 
   // go backwards from last day
-  for (let day = 7; day > 0; day--) {
+  for (let day = NUMBER_OF_DAYS; day > 0; day--) {
     
     // find the best job
     const availableJobs = getAvailableJobsForDay(day, jobs);
