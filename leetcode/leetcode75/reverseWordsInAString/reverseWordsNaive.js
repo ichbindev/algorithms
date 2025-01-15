d64cd5ff4f63a6d7c3ad140fa21962c6034c8e52
ichbindev <cmlinac@gmail.com>
2025-01-15 13:33:46 -0600

    reverse words, initial solution

---
+++ leetcode/leetcode75/reverseWordsInAString/reverseWordsNaive.js
@@ -0,0 +1,23 @@
+/*
+Given an input string s, reverse the order of the words.
+
+A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
+
+Return a string of the words in reverse order concatenated by a single space.
+
+Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
+*/
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+  const words = s.split(/\s+/);
+  const reversedWords = [];
+  for (let i = words.length - 1; i >= 0; i--) {
+      let word = words[i].trim();
+      if (word) reversedWords.push(word);
+  }
+  return reversedWords.join(' ');
+};
