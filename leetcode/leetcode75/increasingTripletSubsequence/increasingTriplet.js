fbc821ae37d1692563196d2cf59563b6de77d091
ichbindev <cmlinac@gmail.com>
2025-01-21 13:27:18 -0600

    clean up increasing triplet solution

--- leetcode/leetcode75/increasingTripletSubsequence/increasingTriplet.js
+++ leetcode/leetcode75/increasingTripletSubsequence/increasingTriplet.js
@@ -7,21 +7,22 @@ Give    t i < j < k and nums[i] < nums[j] < nums[k]. If no such indices exists, 
  * @return {boolean}
  */
 var increasingTriplet = function(nums) {
-  if (nums.length < 3) return false;
-  let largest = nums[nums.length - 1];
-  let smallest = undefined;
-  for (let i = nums.length - 2; i >= 0; i--) {
-      if (!smallest && nums[i] < largest) {
-          smallest = nums[i];
+  // keep track of two things:
+  // smallest: the smallest element we've seen
+  // middleCandidate: the smallest element greater than a previous element
+  // if we find one larger than both, we have a valid triplet of
+  // [smallest', middleCandidate, n], where smallest' is the smallest value 
+  // visited up to that point in the array
+  let smallest = Number.MAX_SAFE_INTEGER;
+  let middleCandidate = Number.MAX_SAFE_INTEGER;
+  for (let n of nums) {
+      if (n <= smallest) {
+          smallest = n;
+      } else if (n <= middleCandidate) {
+          middleCandidate = n;
+      } else {
+          return true;
       }
-      else if (nums[i] > largest) {
-          largest = nums[i];
-      } else if (nums[i] < largest) {
-          if (nums[i] < smallest) {
-              return true;
-          }
-          smallest = nums[i];
-      } 
   }
   return false;
 };
