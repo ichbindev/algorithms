c54e52300bbbe46c4b8645529c97e260e2e86f9b
ichbindev <cmlinac@gmail.com>
2025-02-04 14:16:11 -0600

    compress string, 2 pointer solution

---
+++ leetcode/leetcode75/compressString/compressString.js
@@ -0,0 +1,36 @@
+/*
+    Given an array of characters chars, compress it using the following algorithm:
+
+    Begin with an empty string s. For each group of consecutive repeating characters in chars:
+
+    If the group's length is 1, append the character to s.
+    Otherwise, append the character followed by the group's length.
+    The compressed string s should not be returned separately, but instead, be stored in the input character array chars. Note that group lengths that are 10 or longer will be split into multiple characters in chars.
+
+    After you are done modifying the input array, return the new length of the array.
+
+    You must write an algorithm that uses only constant extra space.
+*/
+
+/**
+ * @param {character[]} chars
+ * @return {number}
+ */
+var compress = function(chars) {
+  let write = 0;
+  let count = 1;
+  for (let idx = 1; idx <= chars.length; idx++) {
+      if (idx < chars.length && chars[idx] === chars[idx - 1]) {
+          count++;
+      } else {
+          chars[write++] = chars[idx - 1];
+          if (count > 1) {
+              for (let c of count.toString()) {
+                  chars[write++] = c;
+              }
+          }
+          count = 1;
+      }
+  }
+  return write;
+}
