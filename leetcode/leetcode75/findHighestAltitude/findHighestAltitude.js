dde9a0b44bb786af9fb990b9d37bb82c0f22a354
ichbindev <cmlinac@gmail.com>
2025-02-06 17:12:34 -0600

    find highest altitude

---
+++ leetcode/leetcode75/findHighestAltitude/findHighestAltitude.js
@@ -0,0 +1,18 @@
+/*
+    There is a biker going on a road trip. The road trip consists of n + 1 points at different altitudes. The biker starts his trip on point 0 with altitude equal 0.
+
+    You are given an integer array gain of length n where gain[i] is the net gain in altitude between points i​​​​​​ and i + 1 for all (0 <= i < n). Return the highest altitude of a point.
+*/
+
+/**
+ * @param {number[]} gain
+ * @return {number}
+ */
+var largestAltitude = function(gain) {
+  let altitude = 0, maxAltitude = 0;
+  gain.forEach(g => {
+      altitude += g;
+      maxAltitude = Math.max(altitude, maxAltitude);
+  })
+  return maxAltitude;
+};
