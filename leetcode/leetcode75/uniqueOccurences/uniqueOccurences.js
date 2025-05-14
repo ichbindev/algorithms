2a6762f54315a31233f03a7910dae109e5aaf0dd
ichbindev <cmlinac@gmail.com>
2025-05-14 11:36:27 -0500

    add unique occurences solution

---
+++ leetcode/leetcode75/uniqueOccurences/uniqueOccurences.js
@@ -0,0 +1,21 @@
+/*
+    Given an array of integers arr, return true if the number of occurrences of each value in the array is unique or false otherwise.
+*/
+
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var uniqueOccurrences = function(arr) {
+  const occurrences = new Map();
+  for (const n of arr) {
+      const count = occurrences.get(n) || 0;
+      occurrences.set(n, count + 1);
+  }
+  const counts = new Set();
+  for (const v of occurrences.values()) {
+      if (counts.has(v)) return false;
+      counts.add(v);
+  }
+  return true;
+};
