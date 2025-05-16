ca1cf17ae621545d46d21d425791ed441bdf3ad2
ichbindev <cmlinac@gmail.com>
2025-05-16 17:35:08 -0500

    remove stars solution

---
+++ leetcode/leetcode75/removeStars/removeStars.js
@@ -0,0 +1,30 @@
+/*
+    You are given a string s, which contains stars *.
+
+    In one operation, you can:
+
+    Choose a star in s.
+    Remove the closest non-star character to its left, as well as remove the star itself.
+    Return the string after all stars have been removed.
+
+    Note:
+
+    The input will be generated such that the operation is always possible.
+    It can be shown that the resulting string will always be unique.
+*/
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeStars = function(s) {
+  const stack = [];
+  for (const c of s) {
+      if (c === '*') {
+          stack.pop();
+      } else {
+          stack.push(c);
+      }
+  }
+  return stack.join('');
+};
