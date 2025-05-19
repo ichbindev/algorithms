2f2ac17dcac269d3b05d65ce8695fa3593c31da7
ichbindev <cmlinac@gmail.com>
2025-05-19 11:42:55 -0500

    clean up delete middle node

--- leetcode/leetcode75/deleteMiddle/deleteMiddle.js
+++ leetcode/leetcode75/deleteMiddle/deleteMiddle.js
@@ -22,23 +22,28 @@
  */
 var deleteMiddle = function(head) {
   if (!head.next) return null;
-  // find one node before the middle
+
+  // find the middle node
   let lag = head,
       lead = head;
   
-  // TODO: combine into one loop
-  // even tho its still O(n) so who cares
-  let size = 0;
-  while (lead !== null) {
-      size++;
-      lead = lead.next;
-  }
-  for (let x = 0; x < Math.floor(size / 2) - 1; x++) {
+  while (lead && lead.next) {
       lag = lag.next;
+      lead = lead.next.next;
+  }
+
+  // to delete the middle node, we need to
+  // find the node before the middle node
+  let current = head,
+      previous = null;
+
+  while (current != lag) {
+      previous = current;
+      current = current.next;
   }
   
-  // remove middle node by making the node before it
-  // point to middle.next
-  lag.next = lag.next?.next || null;
+  // remove middle node by making the previous
+  // node's next point to middle.next
+  previous.next = current.next;
   return head;
 };
