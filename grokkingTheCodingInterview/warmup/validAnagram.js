bd211d81317cf1a6411770b590b715f47d4592d8
ichbindev <cmlinac@gmail.com>
2025-05-21 16:44:03 -0500

    add alt solution for valid anagram

--- grokkingTheCodingInterview/warmup/validAnagram.js
+++ grokkingTheCodingInterview/warmup/validAnagram.js
@@ -24,3 +24,34 @@ class Solution {
     return true;
   }
 };
+
+// solution with only one map, checks parity
+class Solution2 {
+  letterMap = {}
+
+  update(key, updateValue = 1) {
+    const { letterMap } = this;
+    const newCount = (letterMap[key] || 0) + updateValue;
+    if (newCount !== 0) {
+      letterMap[key] = newCount;
+    } else {
+      delete letterMap[key];
+    }
+  }
+
+  isAnagram(s, t) {
+    const { letterMap } = this;
+    if (s.length !== t.length) return false;
+
+    // use the same map to count, increment for s and decrement for t
+    // delete the key if count is at 0
+    for (let i = 0; i < s.length; i++) {
+      this.update(s[i]);
+      this.update(t[i], -1);
+    }
+
+    // if there are any keys left, then the count of that character was
+    // not even in s and t, and thus it's not an anagram
+    return Object.keys(letterMap).length === 0;
+  }
+};
