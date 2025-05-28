72e6005790a8b9ecff8e0163634f0c8809bf0d46
ichbindev <cmlinac@gmail.com>
2025-05-28 11:46:03 -0500

    search bst, iterative and recursive solutions

---
+++ grind75/searchBST/searchBSTRecursive.js
@@ -0,0 +1,25 @@
+/*
+You are given the root of a binary search tree (BST) and an integer val.
+
+Find the node in the BST that the node's value equals val and return the subtree rooted with that node. If such a node does not exist, return null.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+function searchBST(root, val) {
+  if (root === null) return null;
+  if (root.val === val) return root;
+  if (root.val > val) return searchBST(root.left, val);
+  return searchBST(root.right, val);
+};
