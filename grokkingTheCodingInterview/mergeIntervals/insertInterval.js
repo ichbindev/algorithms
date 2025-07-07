218fa084c6994fa531a0ed28f3c06199ccb3348d
ichbindev <cmlinac@gmail.com>
2025-07-07 09:50:54 -0500

    new approach for insert interval

--- grokkingTheCodingInterview/mergeIntervals/insertInterval.js
+++ grokkingTheCodingInterview/mergeIntervals/insertInterval.js
@@ -14,15 +14,32 @@ list that has only mutually exclusive intervals.
 
 class Solution {
   insert(intervals, newInterval) {
-    const insertIndex = intervals.findIndex(x => x.start > newInterval.start);
-    if (insertIndex === -1) {
+    // return this.insertAndMerge(intervals, newInterval);
+    const insertAfter = intervals.findIndex(x => x.start > newInterval.start);
+    if (insertAfter === -1) {
       intervals.push(newInterval);
       return intervals;
-    } else {
-      intervals.splice(insertIndex, 0, newInterval); 
-    }
+    } 
+    intervals.splice(insertAfter, 0, newInterval); 
 
     return this.merge(intervals);
+  }
+
+  insertAndMerge(intervals, newInterval) {
+    // keep non-overlapping intervals before new interval
+    let i = intervals.findIndex(x => x.end >= newInterval.start);
+    if (i === -1) return [...intervals, newInterval];
+    const merged = intervals.slice(0, i);
+
+    // merge overlapping intervals
+    while (i < intervals.length && intervals[i].start <= newInterval.end) {
+      newInterval.start = Math.min(intervals[i].start, newInterval.start);
+      newInterval.end = Math.max(intervals[i].end, newInterval.end);
+      i++;
+    }
+
+    // keep non-overlapping intervals after new interval
+    return merged.concat([newInterval, ...intervals.slice(i)]);
   }
 
   merge(intervals) {
