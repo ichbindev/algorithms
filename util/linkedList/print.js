cbd40b79dce2d88d17700fb1088ff6a67a250102
ichbindev <cmlinac@gmail.com>
2025-07-09 09:48:55 -0500

    flesh out linkedList print

--- util/linkedList/print.js
+++ util/linkedList/print.js
@@ -1,17 +1,23 @@
-// print a linked list w/ cycle detection
-// uses O(n) space
-function print(head) {
-  let out = '';
+// print a linked list w/ optional cycle detection
+// uses O(n) space for cycle detection
+// will stop after 50 by default, manual verification isn't 
+//  useful for lists that big and you probably have a cycle
+function print(head, checkCycle = false, maxPrint = 50) {
+  let out = '',
+      printed = 0;
   const visited = new Set();
-  while (head) {
+  while (head && printed < maxPrint) {
     out += head.val;
-    if (visited.has(head.val)) {
-      out += ' CYCLE'
-      break;
+    if (checkCycle) {
+      if (visited.has(head.val)) {
+        out += ' CYCLE'
+        break;
+      }
+      visited.add(head.val);
     }
-    visited.add(head.val);
     if (head.next) out += ' -> '
     head = head.next;
+    printed++;
   }
   console.log(out)
 }
