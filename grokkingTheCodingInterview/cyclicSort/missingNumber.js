7a76b49589cfa7660175d4903acbe0914c52bb9c
ichbindev <cmlinac@gmail.com>
2025-07-11 09:38:51 -0500

    missing number

---
+++ grokkingTheCodingInterview/cyclicSort/missingNumber.js
@@ -0,0 +1,21 @@
+/*
+We are given an array containing n distinct numbers 
+taken from the range 0 to n. Since the array has only n 
+numbers out of the total n+1 numbers, find the missing number.
+*/
+
+class Solution {
+  findMissingNumber(nums) {
+    let i = 0;
+    while (i < nums.length) {
+      const num = nums[i];
+      if (num !== nums.length && nums[i] !== nums[num]) {
+        [nums[i], nums[num]] = [nums[num], nums[i]];
+      } else {
+        i++;
+      }
+    }
+    const missing = nums.indexOf(nums.length);
+    return missing !== -1 ? missing : nums.length;
+  }
+}
