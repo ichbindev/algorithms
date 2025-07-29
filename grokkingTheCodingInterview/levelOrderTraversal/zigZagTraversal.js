98a13ca1df61a798472d9c250214a6337d40eb89
ichbindev <cmlinac@gmail.com>
2025-07-29 13:48:23 -0500

    zigzag traversal, stack

---
+++ grokkingTheCodingInterview/levelOrderTraversal/zigZagTraversal.js
@@ -0,0 +1,43 @@
+/*
+Given a binary tree, populate an array to represent its zigzag 
+level order traversal. You should populate the values of all nodes 
+of the first level from left to right, then right to left for the 
+next level and keep alternating in the same manner for the following 
+levels.
+
+Constraints:
+- The number of nodes in the tree is in the range [0, 2000].
+- -1000 <= Node.val <= 1000
+*/
+
+/*class TreeNode {
+    constructor(val) {
+      this.val = val;
+      this.left = null;
+      this.right = null;
+    }
+  }*/
+
+  class Solution {
+    traverse(root) {
+      let stack = [],
+          dir = true;
+      const result = [];
+      if (root) stack.push(root);
+      while (stack.length) {
+        const rowVals = [],
+              nextStack = [];
+        while (stack.length) {
+          const { val, left, right } = stack.pop();
+          rowVals.push(val);
+          if (dir && left) nextStack.push(left);
+          if (right) nextStack.push(right);
+          if (!dir && left) nextStack.push(left);
+        }
+        dir = !dir;
+        result.push(rowVals);
+        stack = nextStack;
+      } 
+      return result;
+    }
+  }
