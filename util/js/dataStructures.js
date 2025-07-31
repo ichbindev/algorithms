9637e885790db4eedf950a6af7f2d15a0035c55a
ichbindev <cmlinac@gmail.com>
2025-07-31 12:08:26 -0500

    data structure notes

---
+++ util/js/dataStructures.js
@@ -0,0 +1,26 @@
+///////////////////////////
+// ARRAY
+//////////////////////////
+let array = [];
+array.push(); // append to end
+array.pop(); // remove from end
+array.shift(); // remove from beginning
+array.unshift(); // insert at beginning
+
+///////////////////////////
+// QUEUE
+//////////////////////////
+let queue = [];
+queue.push(); // enqueue
+queue.shift(); // dequeue
+!queue.length; // isEmpty
+queue[0]; // peek
+
+///////////////////////////
+// STACK
+//////////////////////////
+let stack = [];
+stack.push(); // push
+stack.pop(); // pop
+!stack.length; // isEmpty
+stack[stack.length - 1]; // peek
