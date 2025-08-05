370de6f1cbbaf60cc26f6b8b74d9f95b42bb1406
ichbindev <cmlinac@gmail.com>
2025-08-05 09:36:23 -0500

    cleanup next greater element backwards

--- algoexpert/nextGreaterElement/nextGreaterElementBackward.js
+++ algoexpert/nextGreaterElement/nextGreaterElementBackward.js
@@ -1,13 +1,15 @@
 function nextGreaterElement(array) {
   const result = new Array(array.length).fill(-1),
         stack = [];
+
+  stack.peek = function() { return this[this.length - 1]; };
   
   for (let i = 2 * array.length - 1; i >= 0; i--) {
     const idx = i % array.length;
-    while (stack.length && stack[stack.length - 1] <= array[idx]) {
+    while (stack.length && stack.peek() <= array[idx]) {
       stack.pop();
     }
-    if (stack.length) result[idx] = stack[stack.length - 1];
+    if (stack.length) result[idx] = stack.peek();
     stack.push(array[idx]);
   }
 
