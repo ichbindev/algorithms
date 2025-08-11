6752c22e25bd150d73ad4b0a454fc715cb7a088c
ichbindev <cmlinac@gmail.com>
2025-08-11 11:44:49 -0500

    array big o

--- util/js/dataStructures.js
+++ util/js/dataStructures.js
@@ -2,10 +2,10 @@
 // ARRAY
 //////////////////////////
 let array = [];
-array.push(); // append to end
-array.pop(); // remove from end
-array.shift(); // remove from beginning
-array.unshift(); // insert at beginning
+array.push(); // append to end O(1)
+array.pop(); // remove from end O(1)
+array.shift(); // remove from beginning O(n) (O(1) impl firefox)
+array.unshift(); // insert at beginning O(n)
 
 array.map();
 array.reduce();
@@ -21,16 +21,16 @@ array.every();
 // QUEUE
 //////////////////////////
 let queue = [];
-queue.push(); // enqueue
-queue.shift(); // dequeue
-!queue.length; // isEmpty
-queue[0]; // peek
+queue.push(); // enqueue O(1)
+queue.shift(); // dequeue O(n) (O(1) impl firefox)
+!queue.length; // isEmpty O(1)
+queue[0]; // peek O(1)
 
 ///////////////////////////
 // STACK
 //////////////////////////
 let stack = [];
-stack.push(); // push
-stack.pop(); // pop
-!stack.length; // isEmpty
-stack[stack.length - 1]; // peek
+stack.push(); // push O(1)
+stack.pop(); // pop O(1)
+!stack.length; // isEmpty O(1)
+stack[stack.length - 1]; // peek O(1)
