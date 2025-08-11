439bf4fb8d12385c8ce9dcb4aa0e191f9a0fda5c
ichbindev <cmlinac@gmail.com>
2025-08-11 11:41:38 -0500

    min num of vertices to reach all nodes

---
+++ grokkingTheCodingInterview/graphs/minVerticesAllNodes.js
@@ -0,0 +1,30 @@
+/*
+Given a directed acyclic graph with n nodes labeled from 0 to n-1, 
+determine the smallest number of initial nodes such that you can 
+access all the nodes by traversing edges. Return these nodes.
+
+Constraints:
+- 2 <= n <= 10^5
+- 1 <= edges.length <= min(10^5, n * (n - 1) / 2)
+- edges[i].length == 2
+- 0 <= fromi, toi < n
+- All pairs (fromi, toi) are distinct.
+*/
+
+class Solution {
+    findSmallestSetOfVertices(n, edges) {
+        const result = [],
+              hasIncomingEdges = new Array(n).fill(false);
+
+        for (const [, end] of edges) {
+            hasIncomingEdges[end] = true;
+        }
+
+        for (const node of Array(n).keys()) {
+            if (!hasIncomingEdges[node])
+                result.push(node);
+        }
+        
+        return result;
+    }
+}
