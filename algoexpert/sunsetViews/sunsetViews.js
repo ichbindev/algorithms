a09ec76b270e0fc36a38ddaf253a11786eaa644e
ichbindev <cmlinac@gmail.com>
2025-08-14 13:23:16 -0500

    clean up sunset views

--- algoexpert/sunsetViews/sunsetViews.js
+++ algoexpert/sunsetViews/sunsetViews.js
@@ -1,19 +1,23 @@
 function sunsetViews(buildings, direction) {
+  direction === 'WEST' && buildings.reverse();
+  
   const nextHighestBuilding = processBuildings(buildings, direction);
+
+  direction === 'WEST' && nextHighestBuilding.reverse();
+  
   return nextHighestBuilding.reduce((a, c, i) => {
     if (c === -1) a.push(i);
     return a;
   }, []);
 }
 
-function processBuildings(buildings, direction) {
+function processBuildings(buildings) {
   const stack = [],
         nextHighest = new Array(buildings.length).fill(-1);
   
   const peek = function(stack) { return stack[stack.length - 1] };
-  const { start, checkEnd, update } = mapDirection(direction, buildings.length);
   
-  for (let i = start; checkEnd(i); i = update(i)) {
+  for (const i of buildings.keys()) {
     while (stack.length && buildings[peek(stack)] <= buildings[i]) {
       const oldIdx = stack.pop();
       nextHighest[oldIdx] = i;
@@ -24,21 +28,6 @@ function processBuildings(buildings, direction) {
   return nextHighest;
 }
 
-function mapDirection(direction, length) {
-  if (direction ==='EAST') {
-    return {
-      'start': 0,
-      'checkEnd': i => i < length,
-      'update': i => i + 1
-    };
-  } else {
-    return {
-      'start': length - 1,
-      'checkEnd': i => i >= 0,
-      'update': i => i - 1
-    };
-  }
-}
 
 // Do not edit the line below.
 exports.sunsetViews = sunsetViews;
