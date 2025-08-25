d9b721de8ac589eca09ddd62436c1e5ed76ff331
ichbindev <cmlinac@gmail.com>
2025-08-25 13:04:56 -0500

    permutations changing case

---
+++ grokkingTheCodingInterview/subsets/permutationsChangingCase.js
@@ -0,0 +1,27 @@
+/*
+Given a string, find all of its permutations preserving the character sequence 
+but changing case.
+
+Constraints:
+- 1 <= str.length <= 12
+- str consists of lowercase English letters, uppercase English letters, and digits.
+*/
+
+class Solution {
+  findLetterCaseStringPermutations(str) {
+    const permutations = new Set();
+    this.doPermutations('', str, permutations);
+    return [...permutations];
+  }
+
+  doPermutations(done, remaining, results) {
+    if (remaining === '') {
+      results.add(done);
+      return;
+    }
+    const char = remaining[0];
+    const toDo = remaining.substring(1);
+    this.doPermutations(done + char.toUpperCase(), toDo, results);
+    this.doPermutations(done + char.toLowerCase(), toDo, results);
+  }
+}
