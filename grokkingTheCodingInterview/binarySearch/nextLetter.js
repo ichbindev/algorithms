b1ce10295fedde69613fe3b7121aa36ba27ca756
ichbindev <cmlinac@gmail.com>
2025-09-01 15:43:59 -0500

    cleanup next letter

--- grokkingTheCodingInterview/binarySearch/nextLetter.js
+++ grokkingTheCodingInterview/binarySearch/nextLetter.js
@@ -19,24 +19,19 @@ Constraints:
 
 class Solution {
   searchNextLetter(letters, key) {
-console.log(letters, key)
     let start = 0,
         end = letters.length - 1;
-
-    if (key >= letters[end]) return letters[0];
     
     while (start <= end) {
       const mid = Math.floor(start + (end - start) / 2),
             char = letters[mid];
       if (key < char) {
         end = mid - 1;
-      } else if (key > char) {
+      } else if (key >= char) {
         start = mid + 1;
-      } else {
-        return letters[mid + 1];
       }
     }
 
-    return letters[start];
+    return letters[start % letters.length];
   }
 }
