f7a0e5fdc1a92eeeebb9953daa30a495df2a27c7
ichbindev <cmlinac@gmail.com>
2025-09-09 16:22:08 -0500

    kth sorted number in m sorted lists

---
+++ grokkingTheCodingInterview/kWayMerge/kthSmallestNumberMSortedLists.js
@@ -0,0 +1,137 @@
+/*
+Given ‘M’ sorted arrays, find the K’th smallest number among all the arrays.
+*/
+
+class Solution {
+  findKthSmallest(lists, k) {
+    const heap = new Heap((a, b) => a.val - b.val);
+    
+    for (let i = 0; i < lists.length; i++) {
+      heap.push({ 
+                  val: lists[i][0], 
+                  list: i, 
+                  idx: 0 
+                });
+    }
+
+    while (--k) {
+      const { list, idx } = heap.pop();
+      if (idx + 1 < lists[list].length) {
+        heap.push({ 
+                    val: lists[list][idx + 1], 
+                    list, 
+                    idx: idx + 1 
+                  });
+      }
+    }
+
+    return heap.peek().val;
+  }
+}
+
+class Heap {
+  constructor(comparator, fromArray = []) {
+    Object.getOwnPropertyNames(Heap.prototype).forEach((key) => {
+      if (key !== "constructor") {
+        this[key] = this[key].bind(this);
+      }
+    });
+
+    if (!comparator) comparator = (a, b) => a - b;
+
+    this.comparator = comparator;
+    this.heap = this.buildHeap(fromArray);
+  }
+
+  buildHeap(array) {
+    let lastParentIdx = this.calculateParent(array.length - 1);
+    for (let i = lastParentIdx; i >= 0; i--) {
+      this.siftDown(i, array);
+    }
+    return array;
+  }
+
+  calculateChildren(i, arr) {
+    const childOne = i * 2 + 1;
+    const pChildTwo = i * 2 + 2;
+    const childTwo = pChildTwo < arr.length ? pChildTwo : -1;
+    return [childOne, childTwo];
+  }
+
+  calculateParent(i) {
+    return Math.floor((i - 1) / 2);
+  }
+
+  push(value) {
+    const { heap, siftUp } = this;
+    heap.push(value);
+    siftUp(heap.length - 1);
+  }
+
+  size() {
+    return this.heap.length;
+  }
+
+  peek() {
+    return this.heap[0];
+  }
+
+  pop() {
+    const { heap, siftDown, swap } = this;
+    swap(0, heap.length - 1);
+    const smallest = heap.pop();
+    siftDown(0);
+    return smallest;
+  }
+
+  remove(item) {
+    const index = this.heap.indexOf(item);
+    if (index === -1) {
+      return false;
+    }
+    this.heap[index] = this.heap[this.heap.length - 1];
+    this.heap.pop();
+    this.siftDown(index);
+    return true;
+  }
+
+  siftDown(i, arr = this.heap) {
+    const { calculateChildren, comparator, swap } = this;
+    let [childOne, childTwo] = calculateChildren(i, arr);
+    while (childOne < arr.length) {
+      // find smaller child to check swap
+      let swapIdx;
+      if (childTwo === -1 || comparator(arr[childOne], arr[childTwo]) < 0) {
+        swapIdx = childOne;
+      } else {
+        swapIdx = childTwo;
+      }
+
+      // only swap if value is smaller than current index
+      if (comparator(arr[swapIdx], arr[i]) > 0) {
+        break;
+      }
+      swap(i, swapIdx, arr);
+      i = swapIdx;
+      [childOne, childTwo] = calculateChildren(i, arr);
+    }
+  }
+
+  siftUp(i) {
+    const { calculateParent, comparator, heap, swap } = this;
+    let parent = calculateParent(i);
+    while (i > 0 && comparator(heap[i], heap[parent]) < 0) {
+      swap(i, parent, heap);
+      i = parent;
+      parent = calculateParent(i);
+    }
+  }
+
+  swap(i, j, arr = this.heap) {
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+
+  toArray() {
+    return [...this.heap];
+  }
+}
