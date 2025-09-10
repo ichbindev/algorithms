3365a4f853872855a0eae953f4489ef4206ec244
ichbindev <cmlinac@gmail.com>
2025-09-10 10:19:17 -0500

    sort k-sorted array

---
+++ algoexpert/sortKSortedArray/sortKSortedArray.js
@@ -0,0 +1,130 @@
+function sortKSortedArray(array, k) {
+  const heap = new Heap();
+  let idx = 0
+  for (const n of array) {
+    heap.push(n);
+    if (heap.size() > k) {
+      array[idx++] = heap.pop();
+    }
+  }
+  
+  while (!heap.isEmpty()) {
+    array[idx++] = heap.pop();
+  }
+  
+  return array;
+}
+
+class Heap {
+  constructor(comparator, fromArray = []) {
+    Object.getOwnPropertyNames(Heap.prototype).forEach((key) => {
+      if (key !== "constructor") {
+        this[key] = this[key].bind(this);
+      }
+    });
+
+    if (!comparator) comparator = (a, b) => a - b;
+
+    this.comparator = comparator;
+    this.heap = this.buildHeap(fromArray);
+  }
+
+  buildHeap(array) {
+    let lastParentIdx = this.calculateParent(array.length - 1);
+    for (let i = lastParentIdx; i >= 0; i--) {
+      this.siftDown(i, array);
+    }
+    return array;
+  }
+
+  calculateChildren(i, arr) {
+    const childOne = i * 2 + 1;
+    const pChildTwo = i * 2 + 2;
+    const childTwo = pChildTwo < arr.length ? pChildTwo : -1;
+    return [childOne, childTwo];
+  }
+
+  calculateParent(i) {
+    return Math.floor((i - 1) / 2);
+  }
+  
+  isEmpty() {
+    return this.heap.length === 0;
+  }
+
+  push(value) {
+    const { heap, siftUp } = this;
+    heap.push(value);
+    siftUp(heap.length - 1);
+  }
+
+  size() {
+    return this.heap.length;
+  }
+
+  peek() {
+    return this.heap[0];
+  }
+
+  pop() {
+    const { heap, siftDown, swap } = this;
+    swap(0, heap.length - 1);
+    const smallest = heap.pop();
+    siftDown(0);
+    return smallest;
+  }
+
+  remove(item) {
+    const index = this.heap.indexOf(item);
+    if (index === -1) {
+      return false;
+    }
+    this.heap[index] = this.heap[this.heap.length - 1];
+    this.heap.pop();
+    this.siftDown(index);
+    return true;
+  }
+
+  siftDown(i, arr = this.heap) {
+    const { calculateChildren, comparator, swap } = this;
+    let [childOne, childTwo] = calculateChildren(i, arr);
+    while (childOne < arr.length) {
+      // find smaller child to check swap
+      let swapIdx;
+      if (childTwo === -1 || comparator(arr[childOne], arr[childTwo]) < 0) {
+        swapIdx = childOne;
+      } else {
+        swapIdx = childTwo;
+      }
+
+      // only swap if value is smaller than current index
+      if (comparator(arr[swapIdx], arr[i]) > 0) {
+        break;
+      }
+      swap(i, swapIdx, arr);
+      i = swapIdx;
+      [childOne, childTwo] = calculateChildren(i, arr);
+    }
+  }
+
+  siftUp(i) {
+    const { calculateParent, comparator, heap, swap } = this;
+    let parent = calculateParent(i);
+    while (i > 0 && comparator(heap[i], heap[parent]) < 0) {
+      swap(i, parent, heap);
+      i = parent;
+      parent = calculateParent(i);
+    }
+  }
+
+  swap(i, j, arr = this.heap) {
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+
+  toArray() {
+    return [...this.heap];
+  }
+}
+
+// Do not edit the line below.
+exports.sortKSortedArray = sortKSortedArray;
